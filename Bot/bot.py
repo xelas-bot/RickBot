49b010df6b385d80f9700779ea3e00eb46bf1092
IEnjoyEatingCookies <dlz9345@gmail.com>
2020-05-28 16:27:45 -0500

    shrey is super obese like wa

--- Bot/bot.py
+++ Bot/bot.py
@@ -3,21 +3,36 @@ import discord
 import math
 import random
 
+
+# Auth bot
 bot = discord.Client()
-@client.event
+@bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     print('ssp is super obese like 100 tons')
 
-@client.event
+# config options
+with open('config.json') as f:
+    config = json.load(f)
+    global prefix 
+    prefix = config['prefix']
+    f.close()
+
+# bot message
+@bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
     
+    if message.content[0] != prefix:
+        return
     
+    args = message.content.split(' ')
+    print(args)
 
 
 with open("auth.json") as f:
     auth = json.load(f)
     bot.run(auth["token"])
+    f.close()
 
