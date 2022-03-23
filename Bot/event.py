561eb17e56feb4fc815b3425c8dbd5eafac3e199
caleb-chou <haloisawesome412@gmail.com>
2022-03-23 14:17:24 -0500

    added some things for americas route api

--- Bot/event.py
+++ Bot/event.py
@@ -1,3 +1,4 @@
+from wsgiref import headers
 import requests
 
 SUMMONER_ID = "5g7AofZD7-9SPHDovoqHl-wp3oqD7d3hoKjpxyHCeNTtQOU"
@@ -5,10 +6,21 @@ PUUID = "7p-tNgLl0yuLm4LSafNuRtsz2SIpTLdQ5ZW-ril-t5sHMqD6U3hu7XIGWn4UnRoyCCTxCo2
 
 
 print("hello")
-base= "https://na1.api.riotgames.com"
-payload = { 'api_key' : 'RGAPI-1d397228-52bc-446c-8263-22abaa0a9ca9'}
+base_na1= "https://na1.api.riotgames.com"
+base_americas = 'https://americas.api.riotgames.com'
+payload = { 'X-Riot-Token' : 'RGAPI-1d397228-52bc-446c-8263-22abaa0a9ca9'}
 
-champions = base + "/lol/platform/v3/champion-rotations"
-curr_match = base + "/lol/spectator/v4/active-games/by-summoner/" + SUMMONER_ID
-response = requests.get(curr_match,params=payload)
+champions = base_na1 + "/lol/platform/v3/champion-rotations"
+curr_match = base_na1 + "/lol/spectator/v4/active-games/by-summoner/" + SUMMONER_ID
+
+def match_list_by_puuid(puuid):
+    return base_americas + '/lol/match/v5/matches/by-puuid/{puuid}/ids'.format(puuid = puuid)
+
+def match_url_by_id(matchid):
+    return base_americas + '/lol/match/v5/matches/{matchId}'.format(matchId = matchid)
+
+response = requests.get(match_list_by_puuid(PUUID), headers=payload)
 print(response.json())
+latest_match = response.json()[0]
+m = requests.get(match_url_by_id(matchid= latest_match), headers=payload)
+print(m.json())
